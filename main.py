969526e75c6cab62f102f0825dfdf52f7e7667c3
Akinuliola Akinbobola <akinuliola_a@yahoo.com>
2022-05-27 03:14:25 +0100

    clean code

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ def find_anagrams(word):
 
     str1 = input("string1:")
     str2 = input("string2:")
-    sorted_str1 = sorted(str1)
-    sorted_str2 = sorted(str2)
+    # sorted_str1 = sorted(str1)
+    # sorted_str2 = sorted(str2)
 
-    if sorted_str1 == sorted_str2:
+    if sorted(str1) == sorted(str2):
         print("True")
     else:
         print("False")
